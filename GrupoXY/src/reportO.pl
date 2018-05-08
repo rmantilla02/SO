ff824b9c5e85002d32e288f7f5c2bb0c7f9652f6
Juan Camilo Rojas <juankdrums14@gmail.com>
2018-05-08 18:02:15 -0300

    Se inicia reportO

---
+++ GrupoXY/src/reportO.pl
@@ -0,0 +1,41 @@
+#!/bin/perl
+
+
+print "-----------------------------ReportO------------------------------\n";
+print "Ingrese el código del país: ";
+$codPais = <STDIN>;
+chomp($codPais);
+print "Ingrese el código del sistema: ";
+$codSistema = <STDIN>;
+chomp($codSistema);
+print "Ingrese la fecha inicial del período: ";
+$inicialPeriodo = <STDIN>;
+chomp($inicialPeriodo);
+print "Ingrese la fecha final del periódo: ";
+$finalPeriodo = <STDIN>;
+chomp($finalPeriodo);
+
+$maestro = "PPI.mae";
+open(ENTRADA,"<$maestro") || die "ERROR: No se puede abrir el archivo maestro";
+my @PAIS_ID = ();
+my @SIS_ID = ();
+my @CTB_ANIO = ();
+my @CTB_MES = ();
+my @CTB_DIA = ();
+my @CTB_ESTADO = ();
+my @PRES_FE = ();
+my @PRES_ID = ();
+my @PRES_TI = ();
+my @MT_PRES = ();
+my @MT_IMPAGO = ();
+my @MT_INDE = ();
+my @MT_INNODE = ();
+my @MT_DEB = ();
+
+while ($linea = <ENTRADA>)
+{
+	$uno = index($linea,";"); 
+}
+
+
+
