25d4f565655a6aa6aed429baedc3ae85d96bc41b
Juan Camilo Rojas <juankdrums14@gmail.com>
2018-05-15 18:06:34 -0300

    Avance en reportO 2.0

--- GrupoXY/src/reportO.pl
+++ GrupoXY/src/reportO.pl
@@ -34,31 +34,50 @@ my @MT_DEB = ();
 
 my @variables = (); #este array almacenará los valores de cada línea separados
 
+my @ppi = ();
+$i = 0;
+
 while ($linea = <ENTRADA>)
 {
-	$i = 0;
-	$ant = -1;
-	$sub = $linea;
-	while ($i < 13)
-	{
-		$ocurrencia = index($sub, ';');
-		$sig = ocurrencia;
-		if($i == 0)
-		{
-			$size = 1;
-		}
-		else
-		{
-			$size = $sig - $ant - 1;
-		}
-		$sub = substr($sub, $sig + 1);
-		$valor = substr($linea, $ant + 1, $size);
-		$ant = $siguiente;
-		push @variables, $valor;
-		$i = $i + 1;
-	}
-	print "@variables\n";
+	@variables = split(/;/,$linea);
+
+	my %fila = ();
+	$fila{"PAIS_ID"} = $variables[0];
+	$fila{"SIS_ID"} = $variables[1];
+	$fila{"CTB_ANIO"} = $variables[2];
+	$fila{"CTB_MES"} = $variables[3];
+	$fila{"CTB_DIA"} = $variables[4];
+	$fila{"CTB_ESTADO"} = $variables[5];
+	$fila{"PRES_FE"} = $variables[6];
+	$fila{"PRES_ID"} = $variables[7];
+	$fila{"PRES_TI"} = $variables[8];
+	$fila{"MT_PRES"} = $variables[9];
+	$fila{"MT_IMPAGO"} = $variables[10];
+	$fila{"MT_INDE"} = $variables[11];
+	$fila{"MT_INNODE"} = $variables[12];
+	$fila{"MT_DEB"} = $variables[13];
+	
+	push @ppi,{PAIS_ID => $variables[0], SIS_ID => $variables[1], CTB_ANIO => $variables[2], CTB_MES => $variables[3],
+	CTB_DIA => $variables[4], CTB_ESTADO => $variables[5], PRES_FE => $variables[6], PRES_ID => $variables[7],
+	PRES_TI => $variables[8], MT_PRES => $variables[9], MT_IMPAGO => $variables[10], MT_INDE => $variables[11],
+	MT_INNODE => $variables[12], MT_DEB => $variables[13]  } ;
+	
 }
 
+
+#print "@ppi\n";
+#print "@ppi[0]\n";
+#print "@ppi[1]\n";
+#print "@ppi[2]\n";
+#print "@ppi[3]\n";
+#print %ppi[0];
+
+for $href ( @ppi ) {
+    print "{ ";
+    for $role ( keys %$href ) {
+         print "$role=$href->{$role} ";
+    }
+    print "}\n";
+}
 
 
