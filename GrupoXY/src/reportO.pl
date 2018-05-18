b28a30da8bdf85e1e6a2eef5949327eb5851f68d
Juan Camilo Rojas <juankdrums14@gmail.com>
2018-05-18 18:24:48 -0300

    Se implementa la recomendación en los reportes. NOTA: EL archivo agregado p-s.mae en la carpeta src es de prueba.

--- GrupoXY/src/reportO.pl
+++ GrupoXY/src/reportO.pl
@@ -16,21 +16,7 @@ $finalPeriodo = <STDIN>;
 chomp($finalPeriodo);
 
 $maestro = "PPI.mae";
-open(ENTRADA,"<$maestro") || die "ERROR: No se puede abrir el archivo maestro";
-my @PAIS_ID = ();
-my @SIS_ID = ();
-my @CTB_ANIO = ();
-my @CTB_MES = ();
-my @CTB_DIA = ();
-my @CTB_ESTADO = ();
-my @PRES_FE = ();
-my @PRES_ID = ();
-my @PRES_TI = ();
-my @MT_PRES = ();
-my @MT_IMPAGO = ();
-my @MT_INDE = ();
-my @MT_INNODE = ();
-my @MT_DEB = ();
+open(ENTRADA1,"<$maestro") || die "ERROR: No se puede abrir el archivo maestro";
 
 my @variables = (); #este array almacenará los valores de cada línea separados
 
@@ -41,7 +27,7 @@ my @ppi = ();
 
 
 
-while ($linea = <ENTRADA>)
+while ($linea1 = <ENTRADA1>)
 {
 	@variables = split(/;/,$linea);
 	
@@ -50,6 +36,19 @@ while ($linea = <ENTRADA>)
 	PRES_TI => $variables[8], MT_PRES => $variables[9], MT_IMPAGO => $variables[10], MT_INDE => $variables[11],
 	MT_INNODE => $variables[12], MT_DEB => $variables[13]  } ;
 	
+}
+
+
+#SE AVERIGUA EL NOMBRE DEL PAÍS
+#Se lee el archivo p-s.mae
+open(ENTRADA2,"<p-s.mae");
+while($linea = <ENTRADA2>)
+{
+	@variables = split(/-/,$linea);
+	if($variables[0] eq $codPais)
+	{
+		$nombrePais = $variables[1];
+	}
 }
 
 
@@ -64,19 +63,62 @@ for $i(@ppi)
 
 
 #LECTURA DE prestamos.pais:
-$file = "prestamos." . $codPais;
-open(ENTRADA,"<$file");
+$file = "prestamos." . $nombrePais;
+open(ENTRADA3,"<$file");
 my @prestamos = ();
-while ($linea = <ENTRADA>)
+while ($linea = <ENTRADA3>)
 {
 	@variables = split(/;/,$linea);
+
 	
 	push @prestamos, {SIS_ID => $variables[0], CTB_ANIO => $variables[1], CTB_MES => $variables[2], CTB_DIA => $variables[3],CTB_ESTADO => $variables[4], PRES_ID => $variables[5], MT_PRES => $variables[6], MT_IMPAGO => $variables[7], MT_INDE => $variables[8], MT_INNODE => $variables[9], MT_DEB => $variables[10], MT_REST => $variables[11], PRES_CLI_ID => $variables[12], PRES_CLI => $variables[13], INS_FE => $variables[14], INS_USER => $variables[15] } ;
 }
 
-print "$file\n";
-print "$prestamos[0]->{INS_USER}\n";
 
+
+
+#RECOMENDACIÓN
+#Se determina la posición que se va a comparar
+$mayor = "0";
+$i = 0;
+while($linea1 = <ENTRADA1> and $linea2 = <ENTRADA3>)
+{
+	if ($ppi[$i]->{"PRES_ID"} eq $prestamos[$i]->{"PRES_ID"} and $ppi[$i]->{"CTB_ANIO"} eq $prestamos[$i]->{"CTB_ANIO"} and $ppi[$i]->{"CTB_MES"} eq $prestamos[$i]->{"CTB_MES"})
+	{
+		if ($ppi["CTB_DIA"] gt $mayor)
+		{
+			$mayor = $ppi["CTB_DIA"];
+		}
+	}
+	$i = $i + 1;
+}
+
+$i = 0;
+$recomendacion = "holi";
+while($linea1 = <ENTRADA1> and $linea2 = <ENTRADA3>)
+{
+	if ($ppi[$i]->{"PRES_ID"} eq $prestamos[$i]->{"PRES_ID"} and $ppi[$i]->{"CTB_ANIO"} eq $prestamos[$i]->{"CTB_ANIO"} and $ppi[$i]->{"CTB_MES"} eq $prestamos[$i]->{"CTB_MES"} and $ppi[$i] eq $mayor)
+	{
+		if ($ppi[$i]->{"CTB_ESTADO"} eq "SMOR" and $prestamos[$i]->{"CTB_ESTADO"} ne "SMOR")
+		{
+			$recomendacion = "RECAL";
+		}
+		else 
+		{
+			if ($restanteMaestro[$i] le $prestamos[$i]->{"MT_REST"})
+			{
+				$recomendacion = "RECAL";
+			}
+			else
+			{
+				$recomendacion = "NO-RECAL";
+			}
+		}
+	}
+	$i = $i + 1;
+}
+
+print "$recomendacion\n";
 
 
 
